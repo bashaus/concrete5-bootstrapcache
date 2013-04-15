0e70a2d69c8985a4333e327036535d028eb08446
Bashkim Isai <me@bashkim.com.au>
2013-04-15 13:18:57 +0200

    Memcached was not passing $data

--- drivers/memcached.php
+++ drivers/memcached.php
@@ -32,7 +32,7 @@ class BootstrapCache_Driver_Memcached extends BootstrapCache_Driver {
 	}
 
 	public function set($data) {
-		$this->cacher->set($this->getKey());
+		$this->cacher->set($this->getKey(), $data);
 	}
 
 	public function purge() {
